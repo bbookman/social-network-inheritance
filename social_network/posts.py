39ebbd3a507a8630dc6a8c4210b9e4aa7dc012a6
Unknown <bruce.bookman@gmail.com>
2018-12-17 09:33:30 -0800

    working on it

--- social_network/posts.py
+++ social_network/posts.py
@@ -1,12 +1,18 @@
 from datetime import datetime
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text
+        if not timestamp:
+            self.timestamp= datetime.today.strftime(%A , %b %d, %Y)
+        else:
+            self.timestamp = timestamp
+        self.user = None
+        # Tuesday, Jan 10, 2017'
+
+    def set_user(self, user):
+        self.user = user
 
 
 # class TextPost(...):  # Inherit properly
