95f4b744d42e0641d54c77b235d3aae75521cbb0
EC2 Default User <ec2-user@ip-172-31-15-172.us-west-2.compute.internal>
2018-12-17 21:59:27 +0000

    code complete, tests not passing

--- social_network/posts.py
+++ social_network/posts.py
@@ -2,13 +2,10 @@ from datetime import datetime
 
 
 class Post(object):
-    def __init__(self, text, timestamp=None):
+    def __init__(self, text, timestamp = None, user = None):
         self.text = text
-        if not timestamp:
-            self.timestamp = datetime.strftime(datetime.today,'%A , %b %d, %Y') 
-        else:
-            self.timestamp = timestamp
-        self.user = None
+        self.timestamp = timestamp
+        self.user = user
 
     def set_user(self, user):
         self.user = user
@@ -16,24 +13,40 @@ class Post(object):
 
 class TextPost(Post):  # Inherit properly
     def __init__(self, text, timestamp=None):
-        super().__init__(text, timestamp)
+        super(TextPost, self).__init__(text, timestamp)
+
 
     def __str__(self):
         return '@{name}: "{text}\n\t{date}"'.format(name = self.user.name, text = self.text, date = self.timestamp) 
+'''
 
+def __init__(self, text, timestamp=None):
+        super(TextPost, self).__init__(text, timestamp)
 
-class PicturePost(...):  # Inherit properly
+    def __str__(self):
+        return '@{first_name} {last_name}: "{text}"\n\t{date}'.format(
+            first_name=self.user.first_name,
+            last_name=self.user.last_name,
+            text=self.text,
+            date=self.timestamp.strftime("%A, %b %d, %Y"))
+'''
+
+class PicturePost(Post):  # Inherit properly
     def __init__(self, text, image_url, timestamp=None):
-        super().__init__(text, timestamp) # assert str(post2) == '@Kevin Watson: "Sample post text"\n\thttp://fake-domain.com/images/sample.jpg\n\tTuesday, Jan 10, 2017'
+        super().__init__(self) 
+        self.text = text
+        self.timestamp = timestamp
         self.image_url = image_url
         
     def __str__(self):
         return '@{name}: "{text}\n\t{image_url}\n\t{date}"'.format(name = self.user.name, text = self.text, image_url = self.image_url, date = self.timestamp) 
 
 
-class CheckInPost(...):  # Inherit properly
+class CheckInPost(Post):  # Inherit properly
     def __init__(self, text, latitude, longitude, timestamp=None): #'@Kevin Checked In: "Sample post text"\n\t-34.603722, -58.381592\n\tTuesday, Jan 10, 2017'
-        super().__init__(text, timestamp)
+        super().__init__(self)
+        self.text = text
+        self.timestamp = timestamp
         self.longitude = longitude
         self.latitude = latitude
 
