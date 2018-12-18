de02d685ebeaaf6f43b87c290caf31230a9b3bdd
EC2 Default User <ec2-user@ip-172-31-15-172.us-west-2.compute.internal>
2018-12-18 01:52:55 +0000

    still failing

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,21 +1,25 @@
-
+import pdb
 class User(object):
     def __init__(self, first_name, last_name, email):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.posts = []
-        self.followers = []
+        self.following = []
 
     def add_post(self, post):
         self.posts.append(post)
 
     def get_timeline(self):
         timeline = []
-        for follower in self.followers:
-            timeline.append(follower.post)
-        timeline.sort(key= lambda x: x.timestamp)
+        #pdb.set_trace()
+        for follower in self.following:
+            timeline.append(follower.posts)
+        timeline.sort(key= lambda follower: follower.post.timestamp)
         return timeline
 
     def follow(self, other):
-        self.followers.append(other)
+        self.following.append(other)
+    
+    def __str__(self):
+        return '{first} {last} - {email}.\n Following: {following}'.format(first = self.first_name, last = self.last_name, email = self.email)
--- social_network/posts.py
+++ social_network/posts.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Post(object):
     def __init__(self, text, timestamp = None, user = None):
         self.text = text
-        self.timestamp = timestamp
+        self.timestamp = timestamp or datetime.utcnow()
         self.user = user
 
     def set_user(self, user):
@@ -18,35 +18,19 @@ class TextPost(Post):  # Inherit properly
 
     def __str__(self):
         return '@{name}: "{text}\n\t{date}"'.format(name = self.user.name, text = self.text, date = self.timestamp) 
-'''
 
-def __init__(self, text, timestamp=None):
-        super(TextPost, self).__init__(text, timestamp)
-
-    def __str__(self):
-        return '@{first_name} {last_name}: "{text}"\n\t{date}'.format(
-            first_name=self.user.first_name,
-            last_name=self.user.last_name,
-            text=self.text,
-            date=self.timestamp.strftime("%A, %b %d, %Y"))
-'''
 
 class PicturePost(Post):  # Inherit properly
     def __init__(self, text, image_url, timestamp=None):
-        super().__init__(self) 
-        self.text = text
-        self.timestamp = timestamp
+        super(PicturePost, self).__init__(text, timestamp) 
         self.image_url = image_url
         
     def __str__(self):
         return '@{name}: "{text}\n\t{image_url}\n\t{date}"'.format(name = self.user.name, text = self.text, image_url = self.image_url, date = self.timestamp) 
 
-
 class CheckInPost(Post):  # Inherit properly
     def __init__(self, text, latitude, longitude, timestamp=None): #'@Kevin Checked In: "Sample post text"\n\t-34.603722, -58.381592\n\tTuesday, Jan 10, 2017'
-        super().__init__(self)
-        self.text = text
-        self.timestamp = timestamp
+        super(CheckInPost, self).__init__(text)
         self.longitude = longitude
         self.latitude = latitude
 
